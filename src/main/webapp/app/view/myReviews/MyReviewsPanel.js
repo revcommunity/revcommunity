8153700ead9c3a9bdc31db318889a56e509ec851
rosol88 <rosol88@gmail.com>
2013-11-04 18:11:30 +0100

    Poprawka merge'a

--- src/main/webapp/app/view/myReviews/MyReviewsPanel.js
+++ src/main/webapp/app/view/myReviews/MyReviewsPanel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Ext.define('RevCommunity.view.myReviews.MyReviewsPanel' ,{
     extend: 'Ext.Panel',
     alias: 'widget.myreviewspanel',
@@ -19,17 +18,6 @@ Ext.define('RevCommunity.view.myReviews.MyReviewsPanel' ,{
 	    		   marginRight:5
 	    	   }
 	       }
-//	       {
-//	    	   xtype:'reviewspanel',
-////	    	   store:Ext.create('Ext.data.Store',{
-////	    		    model:'RevCommunity.model.Review',
-////	    		    autoLoad:true,
-////				    proxy: {
-////				        type: 'rest',
-////				        url : 'rest/reviews/myReviews'
-////				    }
-////	    	   })
-//	       }
 	],
 	initComponent:function(){
 		var url=null;
@@ -51,37 +39,4 @@ Ext.define('RevCommunity.view.myReviews.MyReviewsPanel' ,{
 	    	   })
 	       });
 	}
-=======
-Ext.define('RevCommunity.view.myReviews.MyReviewsPanel' ,{
-    extend: 'Ext.Panel',
-    alias: 'widget.myreviewspanel',
-	bodyPadding: 5,
-	width: 250,
-	layout: {
-	    type: 'hbox',
-	    align : 'stretch',
-	    pack  : 'start',
-	},
-	mode:'myReviews',
-	title:'Moje recenzje',
-	items:[
-	       {
-	    	   xtype:'userinfopanel',
-	    	   style:{
-	    		   marginRight:5
-	    	   }
-	       },
-	       {
-	    	   xtype:'reviewspanel',
-	    	   store:Ext.create('Ext.data.Store',{
-	    		    model:'RevCommunity.model.Review',
-	    		    autoLoad:true,
-				    proxy: {
-				        type: 'rest',
-				        url : 'rest/reviews/myReviews'
-				    }
-	    	   })
-	       }
-	]
->>>>>>> branch 'master' of https://github.com/revcommunity/revcommunity.git
 });
