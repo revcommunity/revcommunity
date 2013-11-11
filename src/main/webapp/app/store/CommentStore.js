52bb6b6d75122799a97ce42ddfdd0233452ddeac
Dawid Janaszak <janaszak.dawid@gmail.com>
2013-11-11 22:08:32 +0100

    Poprawki w widoku recenzji - dodawanie komentarzy, lajki, style

---
+++ src/main/webapp/app/store/CommentStore.js
@@ -0,0 +1,10 @@
+Ext.define('RevCommunity.store.CommentStore', {
+	extend:'Ext.data.Store',
+	model:'RevCommunity.model.Comment',
+    proxy: {
+        type: 'rest',
+    },
+    setReviewNodeId:function(reviewNodeId){
+    	this.proxy.url="rest/comments/reviewComments/"+reviewNodeId;
+    },
+});
