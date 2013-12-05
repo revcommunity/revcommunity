51b6b4d2fa172015d61b9c72e0ef7ee63f0c91f7
Dawid Janaszak <janaszak.dawid@gmail.com>
2013-12-05 23:55:19 +0100

    Widok produktu: dodawanie recenzji, panel z opisem prod.

--- src/main/webapp/app/view/product/ProductWrapper.js
+++ src/main/webapp/app/view/product/ProductWrapper.js
@@ -1,7 +1,51 @@
 Ext.define('RevCommunity.view.product.ProductWrapper', {
-		    extend:'Ext.container.Container',
-		    alias: 'widget.productwrapper',
-		    layout: {
-		    	    type: 'hbox',
-		    }
+	extend : 'Ext.container.Container',
+	alias : 'widget.productwrapper',
+	layout : {
+		type : 'hbox',
+	},
+	productData : null,
+	items : [ {
+		xtype : 'container',
+		flex : 1,
+		layout : {
+			type : 'vbox',
+			align : 'stretch',
+			pack : 'start',
+		},
+		items : [ {
+			title : 'Opis produktu',
+			xtype : 'panel',
+			cls : 'rev-review-panel',
+			bodyPadding : 5,
+			collapsible : true,
+			collapsed : true,
+
+		}, {
+			title : 'Recenzje',
+			xtype : 'reviewspanel',
+			mode : 'full',
+			tools : [ {
+				type : 'add',
+				tooltip : 'Dodaj recenzję',
+				handler : function(event, toolEl, panel) {
+					var id = this.up('productwrapper').productData.nodeId;
+					location.href = '#reviews/add' + id;
+				}
+			} ],
+			collapsible : true,
+			collapsed : false,
+		} ]
+	}, {
+		xtype : 'productpanel',
+		data : this.productData,
+	} ],
+	initComponent : function() {
+		this.callParent(arguments);
+		this.down('reviewspanel').getStore().setProductId(
+				this.productData.nodeId);
+		this.down('reviewspanel').getStore().load();
+		this.down('productpanel').data = this.productData;
+		this.down('panel').html = this.productData.description;
+	}
 });
