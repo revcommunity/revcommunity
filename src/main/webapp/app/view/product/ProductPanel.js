544af471e778aa54ee3042051b66256e78248f14
Dawid Janaszak <janaszak.dawid@gmail.com>
2013-12-06 12:06:46 +0100

    Poprawki w widoku produktu

--- src/main/webapp/app/view/product/ProductPanel.js
+++ src/main/webapp/app/view/product/ProductPanel.js
@@ -1,28 +1,61 @@
-Ext.define('RevCommunity.view.product.ProductPanel' ,{
-    extend: 'Ext.form.Panel',
-    alias: 'widget.productpanel',
-	bodyPadding: 5,
-	border: false,
-	width: 250,
-	tpl: new Ext.XTemplate(
-			'<div class="rev-list-header"><span>{name}</span></div>',
-			'<img src="{mainImage}" class="rev-img-details">',
-			'<div class="rev-product-boxes">',
-			   '<div class="rev-boxes">',
-			      '<div class="rev-box">{reviewCount} recenzji</div>',
-			      '<div class="rev-box">Srednia ocena: {rating}</div>',
-			   '</div>',
-			   '<div class="rev-boxes">',
-			      '<div class="rev-box">Cena: {priceAvg} zł</div>',
-			      '<div class="rev-box">F</div>',
-			  '</div>',
-			'</div>',
-			  '<div class="rev-product-spec rev-border">',
-			  	'<p>Producent: {producer}</p>',
-			  	'<p>Kod produktu: {productCode}</p>',
-			  	'<tpl for="filters">',
-			  		'<p>{name}: {value}</p>',
-			  	'</tpl>',
-			  '</div>'	  
-	)
+Ext.define('RevCommunity.view.product.ProductPanel', {
+	extend : 'Ext.form.Panel',
+	alias : 'widget.productpanel',
+	bodyPadding : 5,
+	border : false,
+	width : 250,
+	initComponent : function() {
+		log(this.data);
+		this.items = [
+				{
+					xtype : 'component',
+					cls : 'rev-list-header',
+					autoEl : {
+						tag : 'div',
+						html : this.data.name,
+					}
+				},
+				{
+					xtype : 'image',
+					src : this.data.mainImage,
+					height : 200,
+					maxWidth : 240,
+				},
+				{
+					xtype : 'container',
+					layout : {
+						type : 'hbox',
+						pack : 'center',
+					},
+					items : [
+							{
+								xtype : 'component',
+								cls : 'rev-box',
+								flex : 1,
+								autoEl : {
+									tag : 'div',
+									html : 'Ocena: ' + this.data.rating + ' ('
+											+ this.data.reviewCount + ')',
+								}
+							},
+							{
+								xtype : 'component',
+								cls : 'rev-box',
+								flex : 1,
+								margin : '0,0,0,0', // overwrite rev-box class
+								autoEl : {
+									tag : 'div',
+									html : 'Cena: ' + this.data.priceAvg
+											+ ' zł',
+								}
+							} ]
+				},
+				{
+					xtype : 'panel',
+					margin : '5, 0, 0, 0',
+					tpl : '<tpl for="filters"><p>{name}: {value}</p></tpl>',
+					data : this.data,
+				}, ];
+		this.callParent(arguments);
+	}
 });
