5460affc88500d88162a3ce2c9ec791755e203f7
Tomek Straszewski <straszewski.tomasz@gmail.com>
2014-01-11 12:59:27 +0100

    Ikony na przyciskach sortujacych

--- src/main/webapp/app/view/components/SortDirection.js
+++ src/main/webapp/app/view/components/SortDirection.js
@@ -2,13 +2,17 @@ Ext.define('RevCommunity.view.components.SortDirection' ,{
 	extend: 'Ext.Button',
     alias: 'widget.sortdirection',
     direction:'DESC',
-    text:'DESC',
+    text:'',
+    icon : "img/desc.png",
     swap:function(){
+    	icon = "";
     	if(this.direction=='ASC'){
     		this.direction='DESC';
+    		icon = 'img/desc.png';
     	}else{
     		this.direction='ASC';
+    		icon = 'img/asc.png';
     	}
-    	this.setText(this.direction);
+    	this.setIcon(icon);
     }
 });
